f13204591cb49de880b2edfab6beb007c1b6d61a
Unknown <marcgonzmont@gmail.com>
2018-04-09 18:13:55 +0200

    Update (WS)

--- main.py
+++ main.py
@@ -6,30 +6,35 @@ def nothing(x):
     pass
 
 def computeThresh(img, thermal, alpha):
-    max_t = thermal.max()
-    min_t = thermal.min()
-    mean_t = (max_t + min_t) // 2
+    min_t = 290
+    max_t = 320
     color = np.zeros_like(img)
     color[:, :, 2] = 255
     win_name = 'Thermal threshold'
     bar1 = 'Min (K)'
     bar2 = 'Max (K)'
     cv2.namedWindow(win_name)
-    cv2.createTrackbar(bar1, win_name, min_t, mean_t, nothing)
-    cv2.createTrackbar(bar2, win_name, mean_t, max_t, nothing)
-    # result = img.copy()
+    cv2.createTrackbar(bar1, win_name, min_t, max_t, nothing)
+    cv2.createTrackbar(bar2, win_name, min_t, max_t, nothing)
     while (1):
         k = cv2.waitKey(1) & 0xFF
         if k == 27:
             break
+
         min = cv2.getTrackbarPos(bar1, win_name)
         max = cv2.getTrackbarPos(bar2, win_name)
 
+        if min < min_t:
+            min = min_t
+        elif max > max_t:
+            max = max_t
+
         mask = cv2.inRange(thermal, min, max, cv2.THRESH_BINARY)
+        # mask = np.where(mask == 255)
         mask = cv2.bitwise_and(cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR), color)
-        # mask = np.where(mask[:,:,:] == 255)
-        result = cv2.addWeighted(img, alpha, mask, 1-alpha, 0)
+        result = cv2.addWeighted(img, alpha, mask, 1 - alpha, 0)
         cv2.imshow(win_name, result)
+
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
